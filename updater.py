fb2a89cbba1b2ea3f8429710100581643d034872
CVL Lycee Jean Renou <95042375+JRUCVL@users.noreply.github.com>
2022-08-13 14:40:51 +0200

    Add files via upload

--- updater.py
+++ updater.py
@@ -6,27 +6,37 @@ import tkinter.ttk
 ##Fonction de verification de la version installée et de recherche de mises à jour
 def trouverMAJ():
     global label
-    with open(file="version.txt",mode='r') as versionActuelle:
-        versioninstallee = versionActuelle.read()
-        wget.download('https://jrucvl.github.io/CAPalgorythm/version.txt', 'server-version.txt')
-        with open(file="server-version.txt", mode="r") as versionServer:
-            derniereversion = versionServer.read()
-            if derniereversion == versioninstallee:
+    try:
+        with open(file="version.txt",mode='r') as versionActuelle:
+            versioninstallee = versionActuelle.read()
+            wget.download('https://jrucvl.github.io/CAPalgorythm/version.txt', 'server-version.txt')
+            with open(file="server-version.txt", mode="r") as versionServer:
+                derniereversion = versionServer.read()
+                if derniereversion == versioninstallee:
+                    versionServer.close()
+                    remove(path='server-version.txt')
+                    quit()
+                else:
+                    label = tkinter.Label(iug, text = "Mise à jour...")
+                    remove(path = 'main.py')
+                    wget.download("https://jrucvl.github.io/CAPalgorythm/main.py", 'main.py')
+                    with open(file='version.txt', mode = "w") as version:
+                        version.writelines(derniereversion)
+                        version.close()
+                    remove(path="logo.png")
+                    wget.download("https://jrucvl.github.io/CAPalgorythm/logo.png", 'logo.png')
                 versionServer.close()
-                remove(path='server-version.txt')
-                quit()
-            else:
-                label = tkinter.Label(iug, text = "Mise à jour...")
-                remove(path = 'main.py')
-                wget.download("https://jrucvl.github.io/CAPalgorythm/main.py", 'main.py')
-                with open(file='version.txt', mode = "w") as version:
-                    version.writelines(derniereversion)
-                    version.close()
-                    remove("logo.png")
-                wget.download("https://jrucvl.github.io/CAPalgorythm/logo.png", 'logo.png')
-            versionServer.close()
-    remove(path='server-version.txt')
-    quit()
+        remove(path='server-version.txt')
+        quit()
+    except FileNotFoundError:
+        with open(file = "version.txt", mode = 'a+') as defaultversion:
+            defaultversion.writelines("0.0.0.2022")
+        retry()
+
+
+def retry():
+    trouverMAJ()
+    return
 
 
 iug = tkinter.Tk()
@@ -35,6 +45,3 @@ progbar = tkinter.ttk.Progressbar(iug, mode="indeterminate", length=500)
 label.pack()
 progbar.start()
 trouverMAJ()
-progbar.stop()
-progbar.pack()
-iug.mainloop()
