35d05c0bbab60c9abfbe28d54717841b89067abb
Xabi08YT <xabigoity@gmail.com>
2023-10-28 15:44:13 +0200

    Removed PythonEnv, started home page for V2 and made part of backend for V2

--- PythonEnv/App/PyScripter/python_init.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: UTF-8 -*-
-#  This file will be run on startup and when a remote Python engine
-#  gets initialized
-
-#  Add import statements or anything else you want here
-#import sys
--- PythonEnv/App/Scripts/start_script.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-REM This script is automatically executed on console start
-Title Portable Python 3.10.5 Console
-REM PRINT python --version
-python -c "import sys; print('Python' + sys.version)"
-doskey pip=python -m pip $*
-REM Add Scripts Path
-REM SET PATH=%PYTHONPATH%\Scripts;%PATH%
