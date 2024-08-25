3b1c023fd65df32db5c763caf9509eb426db1464
Xabi GOÏTY <xabigoity@gmail.com>
2024-08-25 10:44:36 +0000

    Added forgotten fiels

---
+++ installer.py
@@ -0,0 +1,5 @@
+PACKAGES_LINUX = "libxcb-cursor0 build-essential libgl1-mesa-dev libcairo2-dev pkg-config python3-dev libgirepository1.0-dev python3.12-venv"
+NEEDED_LIB_LINUX = "distro"
+SUPPORTED_LINUX_DISTRO = ["debian","ubuntu","fedora","vanilla","linuxmint","raspbian",]
+PORTABLE_ENV_WINDOWS = ""
+
